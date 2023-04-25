ae7505122a18fdedd8f60a93c279283d89f9f64b
Hollin A <ariyawansahollin@gmail.com>
2023-04-26 02:36:58 +0530

    designed the vehicle details page

--- react-frontend/src/components/VehiclePhotoCarousel.js
+++ react-frontend/src/components/VehiclePhotoCarousel.js
@@ -1,54 +1,55 @@
-import Slider from "react-slick";
+import React, { useState } from "react";
 
-import "./VehiclePhotoCarousel.css";
+import classes from "./VehiclePhotoCarousel.module.css";
 
 // Set the baseUrl to the image URL endpoint
 const baseUrl = "https://picsum.photos";
 
 const VehiclePhotoCarousel = () => {
+  const [previewPhoto, setPreviewPhoto] = useState(
+    `${baseUrl}/800/600?random=1`
+  );
+
   const photoArray = [
-    { id: 1, link: `${baseUrl}/800/600?random=1` },
-    { id: 2, link: `${baseUrl}/800/600?random=2` },
-    { id: 3, link: `${baseUrl}/800/600?random=3` },
-    { id: 4, link: `${baseUrl}/800/600?random=4` },
+    { id: 1, link: `${baseUrl}/800/800?random=1` },
+    { id: 2, link: `${baseUrl}/500/600?random=2` },
+    { id: 3, link: `${baseUrl}/200/600?random=3` },
+    { id: 4, link: `${baseUrl}/800/500?random=4` },
   ];
 
-  const settings = {
-    customPaging: function (i) {
-      return (
-        <a>
-          <div className="vehicleCarouselThumbnailDiv">
-            <img
-              src={`${baseUrl}/800/600?random=${i + 1}`}
-              alt={`Slide ${i + 1}`}
-              className="vehicleCarouselThumbnailPhoto"
-            />
-          </div>
-        </a>
-      );
-    },
-    dots: true,
-    dotsClass: "slick-dots slick-thumb",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+  const handleClick = (id) => {
+    const selectedPhoto = photoArray.find((photo) => photo.id === id);
+    if (selectedPhoto) {
+      setPreviewPhoto(selectedPhoto.link);
+    }
   };
+
   return (
-    <div className="vehicleCarousel">
-      <Slider {...settings}>
-        {photoArray.map((photoItem) => {
+    <div className={classes.vehicleCarousel}>
+      <div className={classes.MainPhotoDiv}>
+        <img
+          src={previewPhoto}
+          className={classes.MainPhoto}
+          alt="vehicle photo"
+        />
+      </div>
+      <div className={classes.thumbnailDiv}>
+        {photoArray.map((photo) => {
           return (
-            <div key={photoItem.id} className="vehicleCarouselMainPhotoDiv">
+            <div
+              className={classes.thumbnailPhotoDiv}
+              key={photo.id}
+              onClick={() => handleClick(photo.id)}
+            >
               <img
-                src={photoItem.link}
-                className="vehicleCarouselMainPhoto"
-                alt={`Slide ${photoItem.id}`}
+                src={photo.link}
+                className={classes.thumbnailPhoto}
+                alt="thumbnail vehicle photos"
               />
             </div>
           );
         })}
-      </Slider>
+      </div>
     </div>
   );
 };
