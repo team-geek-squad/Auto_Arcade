2dc9c3cf97b2b5e5e8c0c51bb9a0e3d498c5c594
Hollin A <ariyawansahollin@gmail.com>
2023-04-26 02:43:09 +0530

    modification on the vehicle details page

--- react-frontend/src/components/VehiclePhotoCarousel.js
+++ react-frontend/src/components/VehiclePhotoCarousel.js
@@ -6,16 +6,14 @@ import classes from "./VehiclePhotoCarousel.module.css";
 const baseUrl = "https://picsum.photos";
 
 const VehiclePhotoCarousel = () => {
-  const [previewPhoto, setPreviewPhoto] = useState(
-    `${baseUrl}/800/600?random=1`
-  );
-
   const photoArray = [
     { id: 1, link: `${baseUrl}/800/800?random=1` },
     { id: 2, link: `${baseUrl}/500/600?random=2` },
     { id: 3, link: `${baseUrl}/200/600?random=3` },
     { id: 4, link: `${baseUrl}/800/500?random=4` },
   ];
+
+  const [previewPhoto, setPreviewPhoto] = useState(photoArray[0].link);
 
   const handleClick = (id) => {
     const selectedPhoto = photoArray.find((photo) => photo.id === id);
